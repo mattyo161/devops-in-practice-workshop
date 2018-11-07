e92c5888a0e7727ffbcf9d537fe7651b9840ae35
Matt Ouellette <mouellette@lexialearning.com>
2018-11-06 23:26:14 -0500

    Update for gcloud auto deploy

--- deploy-glcoud.sh
+++ deploy-glcoud.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -xe
-kubectl apply -f kubernetes-glcoud/mysql.yml
-kubectl apply -f kubernetes-glcoud/web.yml
+# kubectl apply -f kubernetes-glcoud/mysql.yml
+# kubectl apply -f kubernetes-glcoud/web.yml
+
+echo "Deploying pet-db..."
+kubectl apply -f kubernetes-glcoud/mysql.yml --namespace default
+
+IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-devops-workshop-221503}
+echo "Deploying pet-web image version: $IMAGE_VERSION"
+
+cat kubernetes-glcoud/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
